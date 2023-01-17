490e644e8d162c29b6ffd4fea87d2442c23fc523
Denys Hutsal <denyshutsal@gmail.com>
2023-01-17 16:15:47 +0100

    feat: add static data.js

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    machineStartDate: "2021-01-12",
+    endDate: "24 Jan, 2021",
+    machineEndDate: "2021-24-12",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1570462395760-8d708c39e7f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+  },
+  {
+    id: 2,
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://goo.gl/maps/ApFUZJoVa1Kfn19r7",
+    startDate: "27 May, 2021",
+    machineStartDate: "2021-27-05",
+    endDate: "8 Jun, 2021",
+    machineEndDate: "2021-08-06",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    imageUrl:
+      "https://images.unsplash.com/photo-1540448051910-09cfadd5df61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+  },
+  {
+    id: 3,
+    title: "Geirangerfjord",
+    location: "Norway",
+    googleMapsUrl: "https://goo.gl/maps/QGC5EQDDZ7jQPgk6A",
+    startDate: "01 Oct, 2021",
+    machineStartDate: "2021-01-10",
+    endDate: "18 Nov, 2021",
+    machineEndDate: "2021-18-11",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1601439678777-b2b3c56fa627?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  },
+];
+
+export default data;
