dce20d9ba9107f07ed0aaac63c656bde05ca8c6d
Denys Hutsal <denyshutsal@gmail.com>
2023-01-17 16:20:15 +0100

    feat: add date.js/scss

---
+++ src/components/date/Date.js
@@ -0,0 +1,27 @@
+import "./Date.scss";
+
+function Date(props) {
+  return (
+    <div className="date card__date">
+      <span>
+        <time
+          dateTime={`${props.machineStartDate}`}
+          title={`${props.machineStartDate}`}
+        >
+          {props.startDate}
+        </time>
+      </span>
+      -
+      <span>
+        <time
+          dateTime={`${props.machineEndDate}`}
+          title={`${props.machineEndDate}`}
+        >
+          {props.endDate}
+        </time>
+      </span>
+    </div>
+  );
+}
+
+export default Date;
