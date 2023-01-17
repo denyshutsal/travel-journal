fb759289904e6da1be7b6d73c41f0927fabb6182
Denys Hutsal <denyshutsal@gmail.com>
2023-01-17 16:18:25 +0100

    feat: add app.js, app.scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header";
+import Card from "./components/card/Card";
+import data from "./data";
+import "./App.scss";
 
 function App() {
+  const cards = data.map((data) => <Card key={data.id} {...data} />);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main className="main">
+        <div className="container">{cards}</div>
+      </main>
     </div>
   );
 }
